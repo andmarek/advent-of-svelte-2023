b43c3badedcf3934e5ce2ea03f4517b4ca405e67
Andrew Marek <andwmarek@gmail.com>
2023-12-01 20:23:58 -0800

    adding searching and adding to the page

--- src/routes/day-one/+page.svelte
+++ src/routes/day-one/+page.svelte
@@ -1,8 +1,35 @@
-<script>
+<script lang="ts">
     import { onMount } from 'svelte';
 
-    let elvesNames;
-    let searchInput;
+    type Elf = {
+        name: string;
+        tally: number;
+    }
+
+    let elvesNames: Elf[];
+    let displayElvesNames: Elf[];
+
+    let searchInput: string = "";
+
+    let elfToAdd: string;
+    let scoreToAdd: string;
+
+    function handleSearchElf() {
+        if (searchInput.length == 0) {
+            console.log("Search input is empty");
+            displayElvesNames = elvesNames;
+            return;
+        }
+        displayElvesNames = elvesNames.filter(elf => elf.name.includes(searchInput));
+    }
+
+    function handleAddElf() {
+        console.log(elfToAdd) 
+        console.log(scoreToAdd);
+        elvesNames.push({name: elfToAdd, tally: parseInt(scoreToAdd)});
+        elvesNames = elvesNames;
+        displayElvesNames = elvesNames;
+    }
 
     async function fetchData() {
         try {
@@ -11,6 +38,7 @@
                 throw new Error(response.statusText);
             }
             elvesNames = await response.json();
+            displayElvesNames = elvesNames;
         } catch (error) {
             console.error(error);
         }
@@ -27,15 +55,24 @@
     <title>Day One</title>
     <meta name="description" content="Svelte demo app" />
 </svelte:head>
+
 <section class="rootSection">
-    <div class="inputSection"> 
-        <input type="text" placeholder="Filter by name" bind:value={searchInput} />
+    <h2> Add an Elf and Their Score!</h2>
+    <div> 
+        <input type="text" placeholder="Add an Elf Name" bind:value={elfToAdd} />
+        <input type="text" placeholder="Add Their Score" bind:value={scoreToAdd} />
+        <button on:click={handleAddElf}> Add </button>
     </div>
+
+    <div class="inputSection"> 
+        <input type="text" on:input={handleSearchElf} placeholder="Filter by name" bind:value={searchInput} />
+    </div>
+
     <!-- https://advent.sveltesociety.dev/data/2023/day-one.json -->
     {#if elvesNames}
         <h1 class="displayTitle">Present Tally</h1>
         <ul class="elvesNamesList">
-            {#each elvesNames as elfName}
+            {#each displayElvesNames as elfName}
                 <li class="elfNameItem">{elfName.name}: {elfName.tally}</li>
             {/each}
         </ul>
